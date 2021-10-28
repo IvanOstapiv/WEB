f9da95981bf54fddf5f131988f68afdb1bc4d619
IvanOstapiv <65124766+IvanOstapiv@users.noreply.github.com>
2021-10-28 10:09:06 +0300

    Add files via upload

---
+++ zavd3.js
@@ -0,0 +1,83 @@
+$(function(){
+	//Завдання 1
+ /*$('.slide-btn').click(function(){
+ alert("«Була натиснута кнопка з класом slide-btn».");
+ })*/
+	//Завдання 2
+	/*$('.features-grids').click(function(){
+		$('.features-grids img[src*=moto]').slideUp(3000);
+	});*/
+	//Завдання 3
+	/*$('.features-grids').click(function(){
+		$('.features-grids img[src*=moto]').slideToggle(2000);
+	});*/
+	//Завдання 4
+	/*$('.head-moto-img').click(function(){
+	 $(this).fadeToggle(3000,0)
+	 })
+	 $('.features-grid p').click(function(){
+	 $('.head-moto-img').fadeTo(3000,1)
+	 })*/
+	 //Завдання 5
+	 /*$('.header').mouseenter(function(){
+	$('.top-nav ul').slideUp(3000);
+	$('.slide-text').slideUp(3000);
+});
+	$('.header').mouseleave(function(){
+	$('.top-nav ul').slideDown(3000);
+	$('.slide-text').slideDown(3000);
+
+});*/
+	//Завдання 6
+	/*$('.slide-btn').click(function(){
+		$('.slide-btn').text('Оберіть мотоцикл нище на сторінці.');
+	})*/
+	//Завдання 7
+	/*$('p').click(function(){
+		$('.features-grids img[src*=moto]').hide(2000);
+	})*/
+	//Завдання 8
+	/*$('.features-grids #p').click(function(){
+	 $('.features-grids img[src*=moto]').slideToggle(2000, function(){
+	 if($("img[src*=moto]").is(":visible"))
+	 {
+		 $('.features-grids #p').text('Сховати фото');
+	 }
+	 else
+	 {
+		 $('.features-grids #p').text('Показати фото');
+	 }
+	 
+	});
+	});
+});*/
+	//Завдання 9
+	/*$('.blog-post').each(function(){
+	 $(this).mouseenter(function(){
+	$(this).find('span:Hidden').fadeIn(3000);
+});
+	$('.col-md-2 span').mouseleave(function(){
+	$('.categorie').fadeOut(3000);
+
+});
+	});*/
+	//Завдання 10
+	/*$(".contatct-active a").click(function(){
+   
+      if ($(".contatct-active a").text() == "eng"){
+    $(".m1 a").text('Home');
+    $(".m2 a").text('Motorcycles');
+    $(".m3 a").text('Sale');
+    $(".m4 a").text('Forum');
+    $(".m5 a").text('Contacts');
+        $(".contatct-active a").text("ua");
+      } else {
+        $(".contatct-active a").text("eng");
+    $(".m1 a").text('Головна');
+    $(".m2 a").text('Мотоцикли');
+    $(".m3 a").text('Розпродаж');
+    $(".m4 a").text('Форум');
+    $(".m5 a").text('Контакти');
+      }
+    });*/
+	});
